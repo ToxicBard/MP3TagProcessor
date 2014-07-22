d02aab56dbe5b7976201262a255cda9c8b190fb2
ToxicBard <limpbizkitfan943@yahoo.com>
2014-07-22 18:04:32 -0400

    Added reference to CommonTools

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,11 @@
 package main;
 
+import commonTools.CommonTools;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("MP3TagProcessor");
+		System.out.println(CommonTools.isDebugMode());
 	}
 
 }
