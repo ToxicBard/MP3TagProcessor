9721da33aa93d933a9a5385b4dbd93d7fc7b1770
ToxicBard <limpbizkitfan943@yahoo.com>
2014-07-22 18:36:10 -0400

    Added to TODO

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,9 +3,16 @@ package main;
 import commonTools.CommonTools;
 
 public class Main {
-
+	/* 
+	 * TODO Once the load/select/save functionality
+	 * has been abstracted, add it here to select the folder
+	 * TODO When the folder has been selected and loaded,
+	 * then iterate through every file/folder inside it.
+	 * There should be a doStuffFile and doStuffFolder.
+	 * TODO Pick and reference the best mp3 tagging library
+	 * for my purposes and then test it out.
+	 */
 	public static void main(String[] args) {
-		System.out.println(CommonTools.isDebugMode());
 	}
 
 }
