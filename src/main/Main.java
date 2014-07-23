fa87456b2f548716d425cb6795287f33582c0b9a
ToxicBard <limpbizkitfan943@yahoo.com>
2014-07-23 01:54:59 -0400

    Added file iteration

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,18 +1,52 @@
 package main;
 
+import java.io.File;
+
 import commonTools.CommonTools;
+import commonTools.FileTools;
 
 public class Main {
 	/* 
-	 * TODO Once the load/select/save functionality
-	 * has been abstracted, add it here to select the folder
-	 * TODO When the folder has been selected and loaded,
-	 * then iterate through every file/folder inside it.
-	 * There should be a doStuffFile and doStuffFolder.
 	 * TODO Pick and reference the best mp3 tagging library
 	 * for my purposes and then test it out.
 	 */
 	public static void main(String[] args) {
+		runTagBot();
+	}
+	
+	private static void runTagBot(){
+		File rootDirectory = FileTools.selectSavedDirectory("Select MP3 Directory",  "cfg/mp3directory.cfg");
+		
+		traverseDirectory(rootDirectory.listFiles());
+	}
+	
+	/*
+	 * Loop through each file/folder in the selected directory
+	 * and do the appropriate action for each file/folder.
+	 */
+	private static void traverseDirectory(File[] myFiles){
+		for(File loopFile : myFiles){
+			
+			if(loopFile.isFile()){
+				doStuffFile(loopFile);
+			}
+			
+			if(loopFile.isDirectory()){
+				doStuffFolder(loopFile);
+				
+				if(loopFile.listFiles() != null){
+					traverseDirectory(loopFile.listFiles());
+				}
+			}
+		}
+	}
+	
+	private static void doStuffFile(File myFile){
+		System.out.println("File: " + myFile.getAbsolutePath());
+	}
+	
+	private static void doStuffFolder(File myFolder){
+		System.out.println("Folder: " + myFolder.getAbsolutePath());
 	}
 
 }
